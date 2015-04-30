a836fe38dcc6894eacb26f55d5340a00491f9215
addieM <admirmonteiro@icloud.com>
2015-04-30 00:03:04 -0400

    Copied the code over that needed to be there

--- client.py
+++ client.py
@@ -1,1 +1,37 @@
 __author__ = 'admirmonteiro'
+from socket import _socketobject
+
+__author__ = 'Jesus is King'
+
+HOST, PORT = "172.30.26.220", 3723
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = (HOST, PORT)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+sock.connect(server_address)
+print 'have connected'
+try:
+
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print >>sys.stderr, 'sending "%s"' % message
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    sock.close()
