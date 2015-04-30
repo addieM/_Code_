7d59d398e8c3ca1c79e723c2c2ca2b300fbc3c77
addieM <admirmonteiro@icloud.com>
2015-04-30 03:31:11 -0400

    Edison Server Code implementation 3

--- Edison_Server.py
+++ Edison_Server.py
@@ -4,7 +4,10 @@ __author__ = 'admirmonteiro is king for the real King'
 import socket
 import sys
 import csv
+import os
 import datetime
+import _struct
+import time
 
 
 # Create a TCP/IP socket
@@ -20,22 +23,29 @@ print>>sys.stderr, 'bind complete, no worries'
 # Listen for incoming connections
 sock.listen(1)
 print>>sys.stderr, 'socket is now listening'
-
+savePath = '/home/root/Coding/Python/Income_Data'
 # Wait for a connection, until it is gotten
 while True:
     print >>sys.stderr, 'waiting for a connection'
     connection, client_address = sock.accept()
     try:
         print >>sys.stderr, 'connection from', client_address
-
         # Receive the data in bytes of 1024 bytes
+        totalData = []
         while True:
             data = connection.recv(8192) # will always return whenever it receives data
-            print >>sys.stderr, 'received "%s"' % data
-
-            if data:
-              print data
-                #name_File = datetime.datetime.now().time()
+            if not data:
+                break
+                connection.close()
+            totalData.append(data)
+        # return ''.join(totalData)
+        print >>sys.stderr, 'received "%s"' % data
+        print('received')
+        dirP = os.getcwd()
+        if dirP == '/home/root/Coding/Python/':
+            name_File = datetime.datetime.now().time()
+            os.chdir(savePath)
+            print savePath
                 # fid = open('test.csv', 'wb')
                 # try:
                 #     for item in data:
@@ -44,9 +54,6 @@ while True:
                 # except:
                 #     print('errors happened while trying to write to the csv file')
                 # connection.sendall(data)
-            else:
-                connection.close()
     finally:
-             print >>sys.stderr, 'finished the socket'
-
+        print >>sys.stderr, 'finished the socket'
 
--- client.py
+++ client.py
@@ -7,6 +7,7 @@ HOST, PORT = "172.30.26.220", 3723
 
 import socket
 import sys
+from scipy.io.wavfile import read
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,13 +16,16 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (HOST, PORT)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
+input_data = read("Voice 005.wav")
+audio = input_data[1]
 print 'have connected'
 try:
 
     # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
+    message =  audio #'This is the message.  It will be repeated.'
+    # print >>sys.stderr, 'sending "%s"' % message
     sock.sendall(message)
+    sock.send(message)
 
     # Look for the response
     amount_received = 0
@@ -31,7 +35,8 @@ try:
         data = sock.recv(16)
         amount_received += len(data)
         print >>sys.stderr, 'received "%s"' % data
-
+except:
+    print('something failed sending data')
 finally:
     print >>sys.stderr, 'closing socket'
     sock.close()
